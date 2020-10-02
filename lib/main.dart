3b7c309eec9861405c49ec0a61fdb43dc29c2e60
karine sobrinho <karine.silva.sobrinho@gmail.com>
2020-10-02 17:17:04 -0300

    AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,34 @@ const request = 'https://api.hgbrasil.com/finance?key=ea21f598';
 
 void main() async {
 
-  http.Response response = await http.get(request);
-  print(json.decode(response.body));
+  print(await getData());
 
   runApp(MaterialApp(
     title: 'Conversor de moedas', 
-    home: Container(
+    home: Home()));
+}
 
-    )));
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('\$ Conversor \$'),
+        backgroundColor: Colors.amber[200],
+        centerTitle: true,
+      ),
+      
+    );
+  }
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
