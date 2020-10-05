d717e5c479f3e4c1af83142d6934766584f14d3e
karine sobrinho <karine.silva.sobrinho@gmail.com>
2020-10-05 11:54:07 -0300

    teste de carregamento e erro de dados

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,9 @@ import 'dart:convert';
 const request = 'https://api.hgbrasil.com/finance?key=ea21f598';
 
 void main() async {
-
   print(await getData());
 
-  runApp(MaterialApp(
-    title: 'Conversor de moedas', 
-    home: Home()));
+  runApp(MaterialApp(title: 'Conversor de moedas', home: Home()));
 }
 
 class Home extends StatefulWidget {
@@ -29,7 +26,34 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.amber[200],
         centerTitle: true,
       ),
-      
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case (ConnectionState.none):
+              case (ConnectionState.waiting):
+                return Center(
+                  child: Text(
+                    'Carregando dados',
+                    style: TextStyle(color: Colors.amber[200], fontSize: 25.0),
+                    textAlign: TextAlign.center,
+                  ),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      'Erro ao carregar dados',
+                      style:
+                          TextStyle(color: Colors.amber[200], fontSize: 25.0),
+                      textAlign: TextAlign.center,
+                    ),
+                  );
+                } else {
+                  return Container(color: Colors.green);
+                }
+            }
+          }),
     );
   }
 }
